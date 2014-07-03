a86542ef8d4f63923d80f1e9285c42b39eceb4ef
Richard Tubbs <richard@sh-tsquib-lt7.stretchline.local>
2014-07-03 13:26:08 +0100

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -101,7 +101,7 @@ static const int GRID_COLUMNS = 10;
 - (BOOL)isIndexValidForX:(int)x andY:(int)y
    {
        BOOL isIndexValid = YES;
-       if(x < 0 | | y < 0 | | x >= GRID_ROWS || y >= GRID_COLUMNS)
+       if(x < 0 || y < 0 || x >= GRID_ROWS || y >= GRID_COLUMNS)
        {
            isIndexValid = NO;
        }
